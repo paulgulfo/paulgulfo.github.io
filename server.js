4f333ee5d8762a73be21c4a41a86f9195c96edca
gulfopaul3@gmail.com <gulfopaul3@gmail.com>
2026-07-03 17:30:27 +0800

    Update chatbot

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function generateLocalReply(message) {
     if (/lawrence|tt|sobrang laki/.test(text)) {
         return 'sobrang laki ng TT ni Lawrence';
     }
-    if (/ashiley|kabayo|aley|asiley|oyabak/.test(text)) {
+    if (/ashiley|kabayo|aley|si asiley ay|oyabak/.test(text)) {
         return 'kabayo or oyabak si ashiley hehe';
     }
 
