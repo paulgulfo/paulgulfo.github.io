aeb827071ce3f0cda31266ff6a7868b321b2d2c8
gulfopaul3@gmail.com <gulfopaul3@gmail.com>
2026-07-03 15:34:09 +0800

    Update chatbot

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ function generateLocalReply(message) {
     if (/|nag gagatas ba si felboy|felboy/.test(text)) {
         return 'Malakas mag kape si felboy';
     }
-     if (/|Anong course ni Aljay|aljay.aljay.jay.taba/.test(text)) {
+     if (/|Anong course ni Aljay|aljay/.test(text)) {
         return 'edi eDOKKKKKKK';
     }
     if (/lawrence|tt|sobrang laki/.test(text)) {
